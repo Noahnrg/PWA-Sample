169352b9faa262ec043e9d11039fdef0843404b7
Noah <noahgomez.r@gmail.com>
2019-08-17 12:36:55 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,21 @@
 /* Service Worker JavaScript File */
 
-/* More info can be found here => https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle */
-
-// The name of the Cache
-const cacheName = 'app-cache-v1';
-
+/* Cache names */
+const cacheNameForApp = 'app-cache-v1';
+const cacheNameForExternalJS = 'app-import-cache-v1';
 
 // Array of Assets to Cache; These assets shouldnt change, or if they do, it's infrequent.
 const AssetsToCache = [
-// './',
-// './index.html',
-// './app.js'
+  // './',
+  // './index.html',
+  // './app.js'
 
-// './css/bootstrap.min.css'
-//    './css/app.css'
+  // './css/bootstrap.min.css'
+  //    './css/app.css'
 ];
 
 
-// The install event is the first event a service worker gets, and it only happens once.
+// The install event is the first event a service worker gets; Happens only once.
 self.addEventListener('install', async function () {
   const cache = await caches.open(cacheName);
   cache.addAll(AssetsToCache);
@@ -26,19 +24,23 @@ self.addEventListener('install', async function () {
 
 self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
+  // TODO: Clear other cache
 });
 
 
 self.addEventListener('fetch', event => {
   const request = event.request;
+  event.respondWith(fetch(request));
+  return;
+
   const url = new URL(request.url);
   if (url.origin === location.origin) {
     event.respondWith(cacheFirst(request));
   } else {
     event.respondWith(networkFirst(request));
+
   }
 });
-
 
 // Custom function that priotizes fetching using the cache
 async function cacheFirst(request) {
@@ -58,3 +60,19 @@ async function networkFirst(request) {
     return cachedResponse || await caches.match('./fallback.json');
   }
 }
+
+
+// Get a 
+//Production cache everything except a checker
+// If checker says diff, reload neccasary
+// No change -> pull from cache everything
+// Network only for backendless data
+
+//testing -> get everything from network
+
+
+// index
+// app.js
+// app.css
+// sw.js
+// manifest
